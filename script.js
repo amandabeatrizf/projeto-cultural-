e1adf239d2361d7273ca60b9af650d4f3ff98784
amanda beatriz <104594079+amandabeatrizf@users.noreply.github.com>
2024-07-31 10:03:28 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,1 +1,98 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [ 
+    { 
+        Questionario: [
+    {
+        enunciado: " 1- Você conhece algum ponto turistico em Campo-Largo ? ",
+        alternativas: [
+            {
+            
+                texto: "Não",
+                afirmacao: " Os pontos turisticos mais cinhecidos de Campo-Largo são: Parque Newton Puppi, City Center Outlet Premium, Museu Histórico de Campo Largo, Morro do Cal, Recanto Salto Boa Vista entre outras opições para o seu passeio!"
+ "
+            },
+            {
+          
+                 texto: " Sim",
+                afirmacao: "Isso é maravilhoso!"
+            }
+        ]
+    },
+    {
+
+
+    
+] 
+
+        [
+    {
+        enunciado: " 2- Gosta de sair em familia em um ambiente livre? ",
+        alternativas: [
+            {
+            
+                texto: "Não",
+                afirmacao: " As vezes passear em lugares sozinho é importante para ter um momento só seu, mas saiba que compania sempre é mais divertido "
+ "
+            },
+            {
+          
+                 texto: " Sim",
+                afirmacao: "Com compania não se pode faltar em um dia divertido de passeio em Campo-Largo"
+            }
+        ]
+    },
+    {
+
+
+    
+] 
+
+            [
+    {
+        enunciado: " 3- Como você se organiza para dar um passeio em um ponto turístico? ",
+        alternativas: [
+            {
+            
+                texto: "Não me organizo, vou na louca",
+                afirmacao: " Corajoso, não podemos dar disculpa para dar uma voltinha, não sabemos o dia de amanhã certo ? "
+ "
+            },
+            {
+          
+                 texto: " Sim eu me organizo",
+                afirmacao: " Planejador, sempre se organizar é tudo, pois assim saimos mais tranquilos para um passeio relaxante. "
+            }
+        ]
+    },
+    {
+
+
+    
+]  
+             [
+    {
+        enunciado: " 4- Você já fez alguma atividade em campo ao ar livre? ",
+        alternativas: [
+            {
+            
+                texto: "Não",
+                afirmacao: " Se não, você pode começar agora ! Volte na questão numero 1 que lá terá dicas de lugares de passeio ao ar livre, bora lá! "
+ "
+            },
+            {
+          
+                 texto: " Sim ",
+                afirmacao: " Parabéns, você tem um perfil turista de Campo-Largo. "
+            }
+        ]
+    },
+    {
+
+
+    
+] 
 
