24b0ec741c797220f41fe7e8b55979ff14f1bd12
amanda beatriz <104594079+amandabeatrizf@users.noreply.github.com>
2024-09-25 10:29:21 -0300

    tualizando

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,14 +4,17 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Não",
-                afirmacao: " Os pontos turisticos mais cinhecidos de Campo-Largo são: Parque Newton Puppi, City Center Outlet Premium, Museu Histórico de Campo Largo, Morro do Cal, Recanto Salto Boa Vista entre outras opições para o seu passeio!"
+                afirmacao: [ " Os pontos turisticos mais cinhecidos de Campo-Largo são: Parque Newton Puppi, City Center Outlet Premium, Museu Histórico de Campo Largo, Morro do Cal, Recanto Salto Boa Vista entre outras opições para o seu passeio!",
                       "Percebeu que em campo largo você consegue entrar e visita esses lugares de graca"
+                      ]
                     }
       {
                  texto: " Sim",
-                afirmacao: "Isso é maravilhoso!"
+                afirmacao: ["Isso e maravilhoso!",
             }               " isto é muito maravilhoso pois sempre que vou conheco as novidades"
-        ]
+                           ]
+        ]   
+         
     },
 
     {
@@ -20,15 +23,17 @@ export const perguntas = [
             {
             
                 texto: "Não",
-                afirmacao: " As vezes passear em lugares sozinho é importante para ter um momento só seu, mas saiba que compania sempre é mais divertido "
+                afirmacao: [" As vezes passear em lugares sozinho é importante para ter um momento só seu, mas saiba que compania sempre é mais divertido " ,
                         " Prefiro passear muitas vezes sozinhos"
+                ]
       }
      ] 
     },
            { 
                  texto: " Sim",
-                afirmacao: "Com compania não se pode faltar em um dia divertido de passeio em Campo-Largo"
+                afirmacao: ["Com compania não se pode faltar em um dia divertido de passeio em Campo-Largo",
                 " amo passear com minha familia e amigos,muito bom sair acompanhada" 
+                ]
            }
       ]
     
@@ -39,13 +44,16 @@ export const perguntas = [
             {
             
                 texto: "Não me organizo, vou na louca",
-                afirmacao: " Corajoso, não podemos dar desculpa para dar uma voltinha, não sabemos o dia de amanhã certo ?"
+                afirmacao: [" Corajoso, não podemos dar desculpa para dar uma voltinha, não sabemos o dia de amanhã certo ?",
             
-            }             " nem sempre podemos ir na louca para viajens e passeios, pense muito bem antes e agir"
+                         " nem sempre podemos ir na louca para viajens e passeios, pense muito bem antes e agir"
+                         ]
+
             {
           
                  texto: " Sim eu me organizo",
-                afirmacao: " Planejador, sempre se organizar é tudo, pois assim saimos mais tranquilos para um passeio relaxante. "
+                afirmacao: [" Planejador, sempre se organizar é tudo, pois assim saimos mais tranquilos para um passeio relaxante. "
+                    ]
             }
         ]
     },
@@ -56,13 +64,16 @@ export const perguntas = [
             {
             
                 texto: "Não",
-                afirmacao: " Se não, você pode começar agora ! Volte na questão numero 1 que lá terá dicas de lugares de passeio ao ar livre, bora lá! "
-                            " se você realmente se interressar é só seguir as intruções"
+                afirmacao:[ " Se nao, voce pode comecar agora ! Volte na questão numero 1 que lá terá dicas de lugares de passeio ao ar livre, bora lá! ",
+                
+                            " se voce realmente se interressar é so seguir as intruções"
+                            ]
              },
             {
           
                  texto: " Sim ",
-                afirmacao: " Parabéns, você tem um perfil turista de Campo-Largo. "
+                afirmacao: [" Parabens, voce tem um perfil turista de Campo-Largo. "
+                    ]
             }
         ]
     },
