ade6f09a607a5a8df93eb2ed9c3e06b48f0164c4
mAARI <103214143+kakashi041@users.noreply.github.com>
2024-10-09 08:49:32 -0300

    atualizando

--- js/perguntas.js
+++ js/perguntas.js
@@ -6,13 +6,18 @@ export const perguntas = [
                 texto: "Não",
                 afirmacao: [ " Os pontos turisticos mais cinhecidos de Campo-Largo são: Parque Newton Puppi, City Center Outlet Premium, Museu Histórico de Campo Largo, Morro do Cal, Recanto Salto Boa Vista entre outras opições para o seu passeio!",
                       "Percebeu que em campo largo você consegue entrar e visita esses lugares de graca"
-                      ]
-                    }
-      {
+                 ],
+                 proxima: 1,
+            },
+            {
                  texto: " Sim",
                 afirmacao: ["Isso e maravilhoso!",
                            " isto é muito maravilhoso pois sempre que vou conheco as novidades"
-                           ]
+                           ],
+                           proxima: 2,
+            },
+                       
+                        
         ]   
          
     },
@@ -25,56 +30,61 @@ export const perguntas = [
                 texto: "Não",
                 afirmacao: [" As vezes passear em lugares sozinho é importante para ter um momento só seu, mas saiba que compania sempre é mais divertido " ,
                         " Prefiro passear muitas vezes sozinhos"
-                ]
-      }
-     ] 
+                ],
+                proxima: 3,
+      },
+    ] 
     },
            { 
                  texto: " Sim",
                 afirmacao: ["Com compania não se pode faltar em um dia divertido de passeio em Campo-Largo",
                 " amo passear com minha familia e amigos,muito bom sair acompanhada" 
-                ]
-           }
+                ],
+                proxima: 4,
+           },
       ]
     
     
     {
-        enunciado: " 3- Como você se organiza para dar um passeio em um ponto turístico? ",
-        alternativas: [
+        enunciado; " 3- Como você se organiza para dar um passeio em um ponto turístico? ",
+        alternativas; [
             {
             
                 texto: "Não me organizo, vou na louca",
                 afirmacao: [" Corajoso, não podemos dar desculpa para dar uma voltinha, não sabemos o dia de amanhã certo ?",
             
                          " nem sempre podemos ir na louca para viajens e passeios, pense muito bem antes e agir"
-                         ]
-
+                         ],
+                         proxima: 5,
+            },
             {
           
                  texto: " Sim eu me organizo",
                 afirmacao: [" Planejador, sempre se organizar é tudo, pois assim saimos mais tranquilos para um passeio relaxante. "
-                    ]
-            }
+                    ],
+                    proxima: 6,
+            },
         ]
-    },
+    }
     
     {
-        enunciado: " 4- Você já fez alguma atividade em campo ao ar livre? ",
-        alternativas: [
+        enunciado; " 4- Você já fez alguma atividade em campo ao ar livre? ",
+        alternativas; [
             {
             
                 texto: "Não",
                 afirmacao:[ " Se nao, voce pode comecar agora ! Volte na questão numero 1 que lá terá dicas de lugares de passeio ao ar livre, bora lá! ",
                 
                             " se voce realmente se interressar é so seguir as intruções"
-                            ]
+                            ],
+                            proxima: 7,
              },
-            {
+             {
           
                  texto: " Sim ",
                 afirmacao: [" Parabens, voce tem um perfil turista de Campo-Largo. "
-                    ]
-            }
+               ],
+            },
         ]
-    },
- ]
+    }
+],
