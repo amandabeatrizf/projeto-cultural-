08957000914ff93c2b9698a0b76664a54ebf1e1c
amanda beatriz <104594079+amandabeatrizf@users.noreply.github.com>
2024-10-09 09:30:04 -0300

    atualizando

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,15 +4,17 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Não",
-                afirmacao: [ " Os pontos turisticos mais cinhecidos de Campo-Largo são: Parque Newton Puppi, City Center Outlet Premium, Museu Histórico de Campo Largo, Morro do Cal, Recanto Salto Boa Vista entre outras opições para o seu passeio!",
-                      "Percebeu que em campo largo você consegue entrar e visita esses lugares de graca"
+                afirmacao: [
+                    " Os pontos turisticos mais cinhecidos de Campo-Largo são: Parque Newton Puppi, City Center Outlet Premium, Museu Histórico de Campo Largo, Morro do Cal, Recanto Salto Boa Vista entre outras opições para o seu passeio!",
+                    "Percebeu que em campo largo você consegue entrar e visita esses lugares de graca"
                  ],
                  proxima: 1,
             },
             {
                  texto: " Sim",
-                afirmacao: ["Isso e maravilhoso!",
-                           " isto é muito maravilhoso pois sempre que vou conheco as novidades"
+                afirmacao: [
+                    "Isso e maravilhoso!",
+                    " isto é muito maravilhoso pois sempre que vou conheco as novidades"
                            ],
                            proxima: 2,
             },
@@ -28,8 +30,9 @@ export const perguntas = [
             {
             
                 texto: "Não",
-                afirmacao: [" As vezes passear em lugares sozinho é importante para ter um momento só seu, mas saiba que compania sempre é mais divertido " ,
-                        " Prefiro passear muitas vezes sozinhos"
+                afirmacao: [
+                    " As vezes passear em lugares sozinho é importante para ter um momento só seu, mas saiba que compania sempre é mais divertido " ,
+                    " Prefiro passear muitas vezes sozinhos"
                 ],
                 proxima: 3,
       },
@@ -37,30 +40,33 @@ export const perguntas = [
     },
            { 
                  texto: " Sim",
-                afirmacao: ["Com compania não se pode faltar em um dia divertido de passeio em Campo-Largo",
-                " amo passear com minha familia e amigos,muito bom sair acompanhada" 
+                afirmacao: [
+                    "Com compania não se pode faltar em um dia divertido de passeio em Campo-Largo",
+                    " amo passear com minha familia e amigos,muito bom sair acompanhada" 
                 ],
                 proxima: 4,
            },
       ]
     
-    
-    {
+    }
+    {    
         enunciado; " 3- Como você se organiza para dar um passeio em um ponto turístico? ",
         alternativas; [
             {
             
                 texto: "Não me organizo, vou na louca",
-                afirmacao: [" Corajoso, não podemos dar desculpa para dar uma voltinha, não sabemos o dia de amanhã certo ?",
-            
-                         " nem sempre podemos ir na louca para viajens e passeios, pense muito bem antes e agir"
+                afirmacao: [ 
+                    " Corajoso, não podemos dar desculpa para dar uma voltinha, não sabemos o dia de amanhã certo ?",
+                    " nem sempre podemos ir na louca para viajens e passeios, pense muito bem antes e agir"
                          ],
                          proxima: 5,
             },
             {
           
                  texto: " Sim eu me organizo",
-                afirmacao: [" Planejador, sempre se organizar é tudo, pois assim saimos mais tranquilos para um passeio relaxante. "
+                afirmacao: [
+                    " Planejador, sempre se organizar é tudo, pois assim saimos mais tranquilos para um passeio relaxante. ",
+                    " E nunca se esquecer de se hidratar antes e durante o passeio "
                     ],
                     proxima: 6,
             },
@@ -73,19 +79,19 @@ export const perguntas = [
             {
             
                 texto: "Não",
-                afirmacao:[ " Se nao, voce pode comecar agora ! Volte na questão numero 1 que lá terá dicas de lugares de passeio ao ar livre, bora lá! ",
-                
+                afirmacao:[ 
+                            " Se nao, voce pode comecar agora ! Volte na questão numero 1 que lá terá dicas de lugares de passeio ao ar livre, bora lá! ",
                             " se voce realmente se interressar é so seguir as intruções"
                             ],
                 },
                 {
           
                  texto: " Sim ",
-                afirmacao: [" Parabens, voce tem um perfil turista de Campo-Largo. ",
-                          
-                            " Esta ansioso para conhecer mais pontos turisticos em Campo-Largo."
-                             ],
-                },
-              ]
-            }
-          ;
+                afirmacao: [
+                    " Parabens, voce tem um perfil turista de Campo-Largo. ",
+                    " Esta ansioso para conhecer mais pontos turisticos em Campo-Largo."
+                    ],
+            },
+        ]
+    }
+]    
